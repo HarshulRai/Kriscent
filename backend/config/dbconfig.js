2c7e14688826aa9bd691f06ad483030bc6d667d7
Harshul Rai <61915312+HarshulRai@users.noreply.github.com>
2024-05-10 13:45:47 +0530

    Create dbconfig.js

---
+++ backend/config/dbconfig.js
@@ -0,0 +1,21 @@
+const { Pool } = require('pg');
+
+
+
+const pool = new Pool({
+
+  user: 'postgres',
+
+  host: 'localhost',
+
+  database: 'kriscent',
+
+  password: '12345',
+
+  port: 5432,
+
+});
+
+
+
+module.exports = pool;
