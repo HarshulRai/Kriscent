94ac202c822fe207ce3a1a30818721bcbcc3320f
Harshul Rai <61915312+HarshulRai@users.noreply.github.com>
2024-05-10 14:30:09 +0530

    Create ProductListing.js

---
+++ frontend/src/components/ProductListing/ProductListing.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import './ProductListing.css';
+
+
+
+const ProductListing = () => {
+
+  return (
+
+    <section className="product-listing" id="products">
+
+      <h2>Featured Products</h2>
+
+      {/* Product listing content */}
+
+    </section>
+
+  );
+
+}
+
+
+
+export default ProductListing;
