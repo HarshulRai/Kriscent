9e7c942876ecc65fcc49f6967979b60a747fa6f5
Harshul Rai <61915312+HarshulRai@users.noreply.github.com>
2024-05-10 14:26:08 +0530

    Create Checkout.js

---
+++ frontend/src/components/Checkout/Checkout.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import './Checkout.css';
+
+
+
+const Checkout = () => {
+
+  return (
+
+    <section className="checkout" id="checkout">
+
+      <h2>Checkout</h2>
+
+      {/* Checkout content */}
+
+    </section>
+
+  );
+
+}
+
+
+
+export default Checkout;
