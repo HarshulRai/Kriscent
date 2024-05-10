b3d2b7776c06052b4fd9c97ee317becfd16a85de
Harshul Rai <61915312+HarshulRai@users.noreply.github.com>
2024-05-10 14:31:34 +0530

    Create Testimonials.js

---
+++ frontend/src/components/Testimonials/Testimonials.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import './Testimonials.css';
+
+
+
+const Testimonials = () => {
+
+  return (
+
+    <section className="testimonials" id="testimonials">
+
+      <h2>What Our Customers Say</h2>
+
+      {/* Testimonials content */}
+
+    </section>
+
+  );
+
+}
+
+
+
+export default Testimonials;
