b93874e5e226bc16b423c523689aea0afe47023a
Harshul Rai <61915312+HarshulRai@users.noreply.github.com>
2024-05-10 14:32:21 +0530

    Create ContactForm.js

---
+++ frontend/src/components/ContactForm/ContactForm.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import './ContactForm.css';
+
+
+
+const ContactForm = () => {
+
+  return (
+
+    <section className="contact-form" id="contact">
+
+      <h2>Contact Us</h2>
+
+      {/* Contact form content */}
+
+    </section>
+
+  );
+
+}
+
+
+
+export default ContactForm;
