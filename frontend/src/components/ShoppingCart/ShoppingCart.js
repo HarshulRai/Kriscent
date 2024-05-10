33c74c17318b25372e1783bc8ecc3c650298e97b
Harshul Rai <61915312+HarshulRai@users.noreply.github.com>
2024-05-10 14:30:53 +0530

    Create ShoppingCart.js

---
+++ frontend/src/components/ShoppingCart/ShoppingCart.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import './ShoppingCart.css';
+
+
+
+const ShoppingCart = () => {
+
+  return (
+
+    <section className="shopping-cart" id="cart">
+
+      <h2>Your Shopping Cart</h2>
+
+      {/* Shopping cart content */}
+
+    </section>
+
+  );
+
+}
+
+
+
+export default ShoppingCart;
